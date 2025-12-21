bfbdfd81e6450dbf16b7ca71567d8c3f705f6b9a
Brutski93 <abrutski@gmail.com>
2025-12-21 13:49:41 +0300

    feat: solve task 12

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -253,8 +253,12 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    if (a.country > b.country) return 1;
+    if (a.country < b.country) return -1;
+    return a.city > b.city ? 1 : -1;
+  });
 }
 
 /** task_13
